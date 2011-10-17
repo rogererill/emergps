e7d52126aed69f4d8d9e0c7d89bfdba62b899578
oriolgand <oriolgand@gmail.com>
2011-10-17 16:39:05 +0000

    Projecte buit, primer commit

---
+++ emergps/src/upc/pxc/emergps/Emergps.java
@@ -0,0 +1,14 @@
+package upc.pxc.emergps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Emergps extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        // PROVA
+    }
+}
