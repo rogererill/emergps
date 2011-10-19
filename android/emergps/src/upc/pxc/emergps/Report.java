78065cb943d6018da8724fa9611bc69a5233f7a0
oriolgand <oriolgand@gmail.com>
2011-10-19 14:28:10 +0000

    Menu + activitats buides

---
+++ android/emergps/src/upc/pxc/emergps/Report.java
@@ -0,0 +1,17 @@
+package upc.pxc.emergps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+import android.util.Log;
+
+public class Report extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.report);
+	}
+}
