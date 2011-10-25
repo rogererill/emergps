17981c24ce87d4c91d5df797c0d93d7cce92ce6b
oriolgand@gmail.com <oriolgand@gmail.com@95dee250-fb0a-a131-4247-63cb6ed111f7>
2011-10-25 19:55:27 +0000

    Mes interficie grafica + mapview

--- trunk/android/emergps/src/upc/pxc/emergps/Incidence.java
+++ trunk/android/emergps/src/upc/pxc/emergps/Incidence.java
@@ -1,3 +1,81 @@
+
+package upc.pxc.emergps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+import android.util.Log;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+
+public class Incidence extends MapActivity {
+   private MapView map;
+   private MapController controller;
+
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.incidence);
+      initMapView();
+      initMyLocation();
+   }
+
+
+   private void initMapView() {
+      map = (MapView) findViewById(R.id.map);
+      controller = map.getController();
+      map.setSatellite(true);
+      map.setBuiltInZoomControls(true);
+   }
+
+   private void initMyLocation() {
+      final MyLocationOverlay overlay = new MyLocationOverlay(this, map);
+      overlay.enableMyLocation();
+      //overlay.enableCompass(); // does not work in emulator
+      overlay.runOnFirstFix(new Runnable() {
+         public void run() {
+            // Zoom in to current location
+            controller.setZoom(8);
+            controller.animateTo(overlay.getMyLocation());
+         }
+      });
+      map.getOverlays().add(overlay);
+   }
+
+   @Override
+   protected boolean isRouteDisplayed() {
+      // Required by MapActivity
+      return false;
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 package upc.pxc.emergps;
 
 import android.app.Activity;
@@ -13,5 +91,7 @@ public class Incidence extends Activity {
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.incidence);
+		
+		
 	}
-}
+}*/
